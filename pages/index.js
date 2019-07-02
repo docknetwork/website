8fcc2d0b38ed8e7d6ed928b0bce6f689a306be41
Sam Hellawell <sshellawell@gmail.com>
2019-07-02 23:42:57 +0100

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+const Index = () => (
+  <div>
+    <Link href="/network">
+      <a>Network</a>
+    </Link>
+    <p>Hello World!</p>
+  </div>
+);
+
+export default Index;
---
+++ pages/network.js
@@ -0,0 +1,5 @@
+export default function Network() {
+  return (
+    <p>Network page - TODO :)</p>
+  );
+}
