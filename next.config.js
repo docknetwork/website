4fb6403ae7f32fe96af32f96babe823937f9e736
samhellawell <sshellawell@gmail.com>
2019-07-09 02:14:18 +0100

    Add eslint and next optimized images plugin

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
-module.exports = {
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
+
+module.exports = Object.assign(withPlugins([
+  [optimizedImages, {}],
+]), {
   exportPathMap: function() {
     return {
       '/': { page: '/' }
     };
   }
-};
+});
