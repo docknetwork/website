ec34fb110822b11baebbc5b9ad0267c8f7face12
samhellawell <sshellawell@gmail.com>
2019-07-12 02:05:22 +0100

    Add favicon and opengraph headers

--- layouts/main.js
+++ layouts/main.js
@@ -1,7 +1,15 @@
-import Head from 'next/head';
 import { ServerStyleSheet } from 'styled-components';
+import Head from 'next/head';
 
 import Footer from '../components/footer';
+
+import favicon16 from '../assets/favicons/favicon-16x16.png';
+import favicon32 from '../assets/favicons/favicon-32x32.png';
+import favicon96 from '../assets/favicons/favicon-96x96.png';
+
+const defaultDescription =
+  'Dock makes it easy for organizations to issue verifiable digital credentials using blockchain and empowers individuals to own and control their digital records.';
+const defaultTitle = 'Micro Credentials & Digital Badges on Blockchain';
 
 export default class MainLayout extends React.Component {
   static getInitialProps({ renderPage }) {
@@ -18,6 +26,10 @@ export default class MainLayout extends React.Component {
     return (
       <>
         <Head>
+          <base href="/" />
+          <link rel="icon" type="image/png" href={favicon32} sizes="32x32" />
+          <link rel="icon" type="image/png" href={favicon96} sizes="96x96" />
+          <link rel="icon" type="image/png" href={favicon16} sizes="16x16" />
           <link href="https://fonts.googleapis.com/css?family=Lato|Open+Sans:400,600,700|Poppins:400,600i,700&display=swap" rel="stylesheet" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta charSet="utf-8" />
@@ -25,6 +37,13 @@ export default class MainLayout extends React.Component {
             name="viewport"
             content="initial-scale=1, maximum-scale=1, user-scalable=no, width=device-width, minimal-ui"
           />
+          <meta name="og:title" content={defaultTitle} />
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta name="wot-verification" content="69f8c3e17a9ca94cf076" />
+          <meta name="fragment" content="!" />
+          <meta name="description" content={defaultDescription} />
+          <meta name="og:description" content={defaultDescription} />
+
           <title>Dock</title>
           <script type="text/javascript">{`
             var _cio = _cio || [];
