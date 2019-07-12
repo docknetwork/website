8521c336a5c2462375881e44c7891f37f9a6d710
samhellawell <sshellawell@gmail.com>
2019-07-12 02:08:13 +0100

    Add inlineImageLimit config option

--- next.config.js
+++ next.config.js
@@ -2,10 +2,10 @@ const withPlugins = require('next-compose-plugins');
 const optimizedImages = require('next-optimized-images');
 const isProd = (process.env.NODE_ENV || 'production') === 'production';
 
-module.exports = withPlugins(
-  [
-    optimizedImages
-  ], {
+module.exports = withPlugins([
+  optimizedImages, {
+    inlineImageLimit: 256
+  }], {
     exportPathMap: function() {
       return {
         '/': { page: '/' }
