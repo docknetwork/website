bfa21f5c17d602128a884b8970915739fb71bced
Sam Hellawell <sshellawell@gmail.com>
2019-07-22 23:57:09 +0100

    Fix eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,10 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-  },
-  extends: 'airbnb',
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  plugins: [
-    'react',
-  ],
+  extends: './node_modules/dev-configs/.eslintrc.browser.react.js',
   rules: {
+    // disable rules from base configurations
+    'no-console': ['error', {allow: ['warn']}],
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
   },
+  globals: {},
 };
